e04ea4e0a6ba230a5a38a3f774cad45507df2747
Emma Papaioannou <emmapapaioannou@outlook.com>
2023-02-16 22:03:09 +0200

    End of Stage 1

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,39 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Appointment with housekeeper',
+    day: '17/02/2023',
+    time: '09:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Supermarket',
+    day: '18/02/2023',
+    time: '11:30',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Medical Treatment',
+    day: '21/02/2023',
+    time: '09:00',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Hair Salon',
+    day: '27/02/2023',
+    time: '15:30',
+    reminder: true,
+  },
+  {
+    id: 5,
+    text: 'First day at my internship',
+    day: '01/03/2023',
+    time: '08:50',
+    reminder: false
+  }
+];
