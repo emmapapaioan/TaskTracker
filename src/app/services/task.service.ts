cc72f1eb35b50c24119dfa22a28962d842fbf088
Emma Papaioannou <emmapapaioannou@outlook.com>
2023-02-17 16:51:42 +0200

    End of Stage 5

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable} from 'rxjs';
 import {Task} from '../Task';
 
+// Set up HTTP headers for sending requests
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json'
@@ -13,24 +14,29 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class TaskService {
+  // URL for the API to retrieve task data
   private apiUrl = 'http://localhost:5000/tasks';
   
   constructor(private http:HttpClient) { }
 
+  // Get a list of tasks from the API
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
   }
 
+  // Delete a task from the API
   deleteTask(task : Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
   }
 
+  // Update the reminder status of a task on the API
   updateTaskReminder(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.put<Task>(url, task, httpOptions);
   }
 
+  // Add a new task to the API
   addTask(task: Task): Observable<Task> {
     return this.http.post<Task>(this.apiUrl, task, httpOptions);
   } 
--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -5,17 +5,21 @@ import { Observable, Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class UiService {
-
+  // Private member variable to keep track of whether the 'Add Task' button is shown or hidden
   private showAddTask: boolean = false;
+
+  // Private subject to communicate changes to the 'showAddTask' variable
   private subject = new Subject<any>(); 
   
   constructor() { }
 
+  // Function to toggle the value of 'showAddTask' and emit the new value through the subject
   toggleAddTask(): void {
     this.showAddTask = !this.showAddTask;
     this.subject.next(this.showAddTask);
   }
 
+  // Function to subscribe to the changes in 'showAddTask' through the subject
   onToggle(): Observable<any> {
     return this.subject.asObservable();
   }
